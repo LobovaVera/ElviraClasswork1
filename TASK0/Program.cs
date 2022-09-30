2eedd2d2e8506c6919d1fbc8475e9bfadc61df43
Vera Lobova <pchilli@yandex.ru>
2022-09-30 20:46:06 +0300

    First task

---
+++ TASK0/Program.cs
@@ -0,0 +1,9 @@
+﻿// квадрат числа
+
+Console.WriteLine(" Введите номер: ");
+int number = int.Parse(Console.ReadLine())11;
+
+
+Console.WriteLine("Квадрат числа " + number + " равен " + number*number);
+
+
